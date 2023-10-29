5e00e8120a2ee9fbe390c793b3cbf3c94d523efd
Khatchi <solomon9ice2014@gmail.com>
2023-10-29 21:05:24 +0100

    ...Task4: Returns the stats of api using storage.count() method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,35 @@
 """Defines api index"""
 
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 @app_views.route('/status')
 def get_status():
     """Returns current status of api"""
     return {'status': 'OK'}
 # Task3 ended here
+
+
+@app_views.route('/stats')
+def get_stats():
+    """
+    Retrieves the number of objs by type,and returns as JSON.
+    """
+
+    count = {
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User),
+    }
+
+    return (count)
