106b06fcb2650467aa91615410cbbfaf8bbc5a7d
Khatchi <solomon9ice2014@gmail.com>
2023-10-30 15:31:43 +0100

    ...Task5: Implemented custom error 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask, make_response
 from api.v1.views import app_views
 from models import storage
 from os import getenv
+import json
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
