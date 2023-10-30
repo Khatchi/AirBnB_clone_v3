f0143215aecc0baa0dcdb933e535d7e8ea140776
Khatchi <solomon9ice2014@gmail.com>
2023-10-30 15:25:07 +0100

    ...Task5: Implemented custom error 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """This modules implements flask api v1 entry point for AirBnB_clone_v3"""
-from flask import Flask
+from flask import Flask, make_response
 from api.v1.views import app_views
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views) # Registers app_views blueprint
+app.register_blueprint(app_views)
+# Registers app_views blueprint
 app.url_map.strict_slashes = False
 
 
@@ -14,6 +15,16 @@ app.url_map.strict_slashes = False
 def close_db(exception):
     """Closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Defines a custom handler for 404 errors"""
+    obj = {'error': 'Not found'}
+    response = make_response(json.dumps(obj), 404)
+    response.headers['Content-Type'] = 'application/json'
+    return (response)
+    # Handles custom error404: Task5
 
 
 if __name__ == "__main__":
